73cdc7b3bd34ebac30a13fff90e7da503d81cc02
smartcsie <smartcsie@gmail.com>
2026-05-08 22:21:25 +0800

    Create Solution_0190.cpp

---
+++ solution/Solution_0190.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int res = 0;
+        for(int i = 1; i <32; i++) {
+            res |= (n & 1);
+            res <<= 1;
+            n >>= 1;
+        }
+        return res;
+    }
+};
