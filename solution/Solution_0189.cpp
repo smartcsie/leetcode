196ea6c22331bc7251b3204f92d309366f42e5f8
smartcsie <smartcsie@gmail.com>
2026-05-11 23:04:17 +0800

    Update Solution_0189.cpp

--- solution/Solution_0189.cpp
+++ solution/Solution_0189.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-        vector<int> temp = nums;
-        for(int i = 0; i < nums.size(); i++) {
-            nums[(i+k) % nums.size()] = temp[i];
+    void reverseString(vector<char>& s) {
+        int i = 0;
+        int j = s.size() - 1;
+        while( i <= j) {
+            swap(s[i++], s[j--]);
         }
     }
 };
