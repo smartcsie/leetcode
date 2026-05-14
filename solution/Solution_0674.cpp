262545f39150a6ec98a98cdcff3087385e2f5097
smartcsie <smartcsie@gmail.com>
2026-05-14 11:12:26 +0800

    Update Solution_0674.cpp

--- solution/Solution_0674.cpp
+++ solution/Solution_0674.cpp
@@ -19,11 +19,15 @@ public:
     int findLengthOfLCIS(vector<int>& nums) {
         int n = nums.size();
         if (n == 0) return 0;
-        vector<int> dp(n, 1);
-        int lcis = 1;
-        for(int i = 1; i < n; i++) {
-            if(nums[i] > nums[i-1]) dp[i] = dp[i-1] + 1;
-            lcis = max(lcis, dp[i]);
+        int lcis = 1;      
+        int count = 1; 
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            lcis = max(lcis, count);
         }
         return lcis;
     }
