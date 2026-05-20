be89686877191ddacbc1f873dfcde890a45be62d
smartcsie <smartcsie@gmail.com>
2026-05-20 13:04:20 +0800

    Add gemini comment

--- solution/Solution_0001.cpp
+++ solution/Solution_0001.cpp
@@ -1,43 +1,30 @@
 /**
- * 解題思路：
- * Time Complexity: O(n) - 
- * Space Complexity: O(n) - 
+ * 題目：1. Two Sum (兩數之和)
+ * 難度：簡單 (Easy)
+ * 描述：給定一個整數陣列 nums 和一個目標值 target，找出和為目標值的兩個整數並回傳索引。
+ * 
+ * 時間複雜度：O(n) - 僅需遍歷陣列一次，雜湊表查詢平均為 O(1)。
+ * 空間複雜度：O(n) - 最壞情況下需將 n-1 個元素存入雜湊表。
+ * 
+ * 解法思路：
+ * 1. 使用一個 unordered_map 紀錄 {數值 : 索引}。
+ * 2. 遍歷 nums[i] 時，計算所需的補數 complement = target - nums[i]。
+ * 3. 檢查 complement 是否已存在於 map 中：
+ *    - 若存在，代表找到了配對，回傳 {map[complement], i}。
+ *    - 若不存在，將當前 nums[i] 存入 map，供後續元素配對。
  */
 
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        std::unordered_map<int, int> numToIndex;
-        for (int i = 0; i < nums.size(); ++i) {
+        unordered_map<int, int> map;
+        for(int i = 0 ; i < nums.size(); i++) {
             int complement = target - nums[i];
-            if (numToIndex.find(complement) != numToIndex.end()) {
-                return {numToIndex[complement], i};
+            if(map.contains(complement)) {
+                return {map[complement] , i};
             }
-            numToIndex[nums[i]] = i;
-        }
-        throw std::invalid_argument("No two sum solution");
-    }
-};
-
-
-/*
-
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        std::unordered_map <int,int> map;
-        for(int i = 0; i < nums.size(); i++) {
-            // 遍历当前元素，并在map中寻找是否有匹配的key
-            auto iter = map.find(target - nums[i]); 
-            if(iter != map.end()) {
-                return {iter->second, i};
-            }
-            // 如果没找到匹配对，就把访问过的元素和下标加入到map中
-            map.insert(pair<int, int>(nums[i], i)); 
+            map[nums[i]] = i;
         }
         return {};
     }
 };
-
-*/
