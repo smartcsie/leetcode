3d562e5fc0bdba7bd6142a161b908179574fb961
smartcsie <smartcsie@gmail.com>
2026-05-29 17:13:41 +0800

    Add gemini comment

--- solution/Solution_0442.cpp
+++ solution/Solution_0442.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        vector<int> ans;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            int absN = abs(nums[i]);
-            int index = absN - 1;
-            // 關鍵修正：確保索引在 [0, n-1] 範圍內
-            if (index >= 0 && index < n) {
-                if (nums[index] < 0) {
-                    ans.push_back(absN);
-                } else {
-                    nums[index] *= -1;
-                }
-            }
+        vector<int> res;
+        for(int num: nums) {
+            int idx = abs(num) - 1;
+            if(nums[idx] < 0) res.push_back(abs(num));
+            else nums[idx] *= -1;
         }
-        
-        return ans;
+        return res;
     }
 };
