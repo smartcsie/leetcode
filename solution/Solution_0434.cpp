aec4c570a9c0a8554ad1df8a6b4501ad22de9efd
smartcsie <smartcsie@gmail.com>
2026-06-05 10:52:23 +0800

    Add gemini comment

---
+++ solution/Solution_0434.cpp
@@ -0,0 +1,22 @@
+/**
+ * 題目：434. Number of Segments in a String (字串中的段數)
+ * 難度：簡單 (Easy)
+ * 描述：計算字串中連續非空白字元組成的區段數量。
+ * * 時間複雜度：O(N) - 遍歷字串一次。
+ * 空間複雜度：O(1) - 僅使用常數空間。
+ * * 優化思路：
+ * 一個區段的開始，必定滿足兩個條件：
+ * 1. 當前字元不是空白。
+ * 2. 當前字元是字串的第一個字元，或者它前一個字元是空白。
+ */
+
+class Solution {
+public:
+    int countSegments(string s) {
+        int count = 0;
+        for(int i = 0; i < s.size(); i++) {
+           if(s[i]!=' ' && (i == 0 || s[i-1] == ' ')) count++;
+        } 
+        return count;
+    }
+};
