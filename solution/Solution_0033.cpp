7010684a38d6f8fadcd4503eeae1fb192a2d9112
smartcsie <smartcsie@gmail.com>
2026-06-18 22:13:18 +0800

    Add gemini comment

--- solution/Solution_0033.cpp
+++ solution/Solution_0033.cpp
@@ -11,31 +11,23 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-        
-        while (left < right) {
-            int mid = left + ((right - left) >> 1);
-            
-            // 判斷左半部是否為有序區間
-            if (nums[mid] >= nums[left]) {
-                // target 是否落在有序的左半部
-                if (target >= nums[left] && target <= nums[mid]) {
-                    right = mid;
-                } else {
+        int n = nums.size();
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) / 1);
+            if (nums[mid] == target) return mid;
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target <= nums[mid])
+                    right = mid - 1;
+                else
                     left = mid + 1;
-                }
-            } 
-            // 若左半部無序，則右半部必然有序
-            else {
-                // target 是否落在有序的右半部
-                if (target > nums[mid] && target <= nums[right]) {
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1])
                     left = mid + 1;
-                } else {
-                    right = mid;
-                }
+                else
+                    right = mid - 1;
             }
         }
-        
-        return (nums[left] == target) ? left : -1;
+        return -1;
     }
 };
