475413ae3e41771ecec5f45039df5d7ffe416c7e
smartcsie <smartcsie@gmail.com>
2026-06-23 17:44:42 +0800

    Update Solution_3950.cpp

--- solution/Solution_3950.cpp
+++ solution/Solution_3950.cpp
@@ -14,12 +14,8 @@ public:
     bool consecutiveSetBits(int n) {
         int count = 0;
         while(n > 0) {
-            if((n & 3) == 3)  {
-                count++;
-                n >>= 2;
-            } else {
-                n >>= 1;
-            }
+            if((n & 3) == 3) count++;
+            n >>= 1;
         }
         return count == 1;
     }
