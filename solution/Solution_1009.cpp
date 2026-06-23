bc23c70c6ad63f23ba0b3b8b3ba56f38a1d75c02
smartcsie <smartcsie@gmail.com>
2026-06-23 22:24:59 +0800

    Update Solution_1009.cpp

--- solution/Solution_1009.cpp
+++ solution/Solution_1009.cpp
@@ -13,11 +13,14 @@
 
 class Solution {
 public:
-    int findComplement(int num) {
-        int mask = ~0;
-        while(mask & num) {
-            mask <<= 1;
+    int bitwiseComplement(int n) {
+        if(n == 0) return 1;
+        int mask = 0;
+        int temp = n;
+        while(temp > 0) {
+            mask = (mask << 1) | 1;
+            temp >>= 1;
         }
-        return ~mask ^ num;
+        return n ^ mask;
     }
 };
