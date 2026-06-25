933661fec95936b66a47ce32b71852f7a01401cb
smartcsie <smartcsie@gmail.com>
2026-06-25 11:03:39 +0800

    Update Solution_0476.cpp

--- solution/Solution_0476.cpp
+++ solution/Solution_0476.cpp
@@ -12,12 +12,10 @@
 class Solution {
 public:
     int findComplement(int num) {
-        int mask = 0;
-        int temp = num;
-        while(temp > 0) {
-            mask= (mask << 1) | 1;
-            temp >>= 1;
+        int mask = ~0;
+        while(num & mask) {
+            mask <<= 1;
         }
-        return mask ^ num;
+        return num ^ ~mask;
     }
 };
