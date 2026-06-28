780c37250f3578a81d70b4466097cc585e26d918
smartcsie <smartcsie@gmail.com>
2026-06-28 22:22:05 +0800

    Update Solution_0434.cpp

--- solution/Solution_0434.cpp
+++ solution/Solution_0434.cpp
@@ -14,9 +14,11 @@ class Solution {
 public:
     int countSegments(string s) {
         int count = 0;
-        for(int i = 0; i < s.size(); i++) {
-           if(s[i]!=' ' && (i == 0 || s[i-1] == ' ')) count++;
-        } 
-        return count;
+        char lastChar = ' ';
+        for(char c : s) {
+            if(c != ' ' && lastChar == ' ')  count++;
+            lastChar = c;
+        }
+        return count ;
     }
 };
