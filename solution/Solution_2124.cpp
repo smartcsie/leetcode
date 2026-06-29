5ea30835b55687f312124fb7c98cfef76df7ba5e
smartcsie <smartcsie@gmail.com>
2026-06-29 11:38:29 +0800

    Add gemini comment

--- solution/Solution_2124.cpp
+++ solution/Solution_2124.cpp
@@ -11,41 +11,9 @@
 class Solution {
 public:
     bool checkString(string s) {
-        return is_sorted(s.begin(), s.end());
-    }
-};
-
-
-/*
-class Solution {
-public:
-    bool checkString(string s) {
-        int aIdx = (s.rfind('a') == std::string::npos) ? -1 : static_cast<int>(s.rfind('a'));
-        int bIdx = (s.find('b') == std::string::npos) ? INT_MAX : static_cast<int>(s.find('b'));
-        return aIdx < bIdx;
-    }
-};
-*/
-
-/*
-class Solution {
-public:
-    bool checkString(string s) {
-        int aIdx = -1;
-        int bIdx = 101;
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] == 'b') {
-                bIdx = i;
-                break;
-            }
+        for(int i = 0; i < s.size() - 1; i++) {
+            if(s[i] == 'b' && s[i + 1] == 'a') return false;
         }
-        for(int i = s.size() - 1; i >= 0 ; i--) {
-            if(s[i] == 'a') {
-                aIdx = i;
-                break;
-            }
-        }
-        return aIdx < bIdx;
+        return true;
     }
 };
-*/
