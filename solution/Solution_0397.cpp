47b597ed259f2859f0460d7bcad412371ec740fe
smartcsie <smartcsie@gmail.com>
2026-07-06 00:41:41 +0800

    Update Solution_0397.cpp

--- solution/Solution_0397.cpp
+++ solution/Solution_0397.cpp
@@ -9,24 +9,18 @@
 class Solution {
 public:
     int integerReplacement(int n) {
-        long long ln = n; // 使用 long long 處理 n+1 的潛在溢位
-        int steps = 0;
-        
-        while (ln > 1) {
-            // 如果是偶數，直接右移 (除以 2)
-            if ((ln & 1) == 0) {
-                ln >>= 1;
-            } 
-            // 如果是 3 或倒數第二位為 0 (例如 ...01)，減 1 較優
-            else if (ln == 3 || (ln & 2) == 0) {
-                ln--;
-            } 
-            // 其他情況 (例如 ...11)，加 1 較優，因為會導致進位產生更多 0
-            else {
-                ln++;
-            }
-            steps++;
+        if (n == INT_MAX) {
+            return 2 + integerReplacement(n / 2 + 1);
         }
-        return steps;
+        int ans = 0;
+        for (; n > 1; ++ans) {
+            if ((n & 1) == 0) 
+                n >>= 1;
+            else if (n == 3 || (n & 2) == 0) 
+                n--;
+            else 
+                n++;
+        }
+        return ans;
     }
 };
