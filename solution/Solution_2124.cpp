3bc1ddf2cfc4c779a12e00d6c69af7feac0e04e6
smartcsie <smartcsie@gmail.com>
2026-07-06 12:55:41 +0800

    Update Solution_2124.cpp

--- solution/Solution_2124.cpp
+++ solution/Solution_2124.cpp
@@ -11,9 +11,6 @@
 class Solution {
 public:
     bool checkString(string s) {
-        for(int i = 0; i < s.size() - 1; i++) {
-            if(s[i] == 'b' && s[i + 1] == 'a') return false;
-        }
-        return true;
+        return s.find("ba") == string::npos;
     }
 };
