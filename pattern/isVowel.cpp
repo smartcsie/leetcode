7fe8e6206c5314a02bbcef9fa28b44290c03afed
smartcsie <smartcsie@gmail.com>
2026-07-13 11:31:35 +0800

    Update isVowel.cpp

--- pattern/isVowel.cpp
+++ pattern/isVowel.cpp
@@ -1,7 +1,4 @@
-bool isVowel(char c) {
-    static const bool lookup[256] = {
-        ['a'] = true, ['e'] = true, ['i'] = true, ['o'] = true, ['u'] = true,
-        ['A'] = true, ['E'] = true, ['I'] = true, ['O'] = true, ['U'] = true
-    };
-    return lookup[(unsigned char)c];
+static bool isVowel(char c) {
+    int lower = c | 0x20;
+    return ((static_cast<int>(lower - '`') << 1) & 2130466) > 0;
 }
