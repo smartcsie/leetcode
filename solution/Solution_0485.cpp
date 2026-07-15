6c0f2155014c16afa401bfd2084018d8642831b4
smartcsie <smartcsie@gmail.com>
2026-07-16 02:06:35 +0800

    Add gemini comment

--- solution/Solution_0485.cpp
+++ solution/Solution_0485.cpp
@@ -15,12 +15,9 @@ public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
         int max_ones = 0;
         int ones = 0;
-        for(int i = 0; i < nums.size(); i++ ) {
-            if(nums[i] & 1) {
-                if(i > 0 && !(nums[i - 1] & 1)) ones = 1;
-                else ones++;
-                max_ones = max(max_ones, ones);
-            }
+        for(const int& num : nums) {
+            ones = (num & 1) ? ones + 1 : 0;
+            max_ones = max(max_ones, ones);
         }
         return max_ones;
     }
