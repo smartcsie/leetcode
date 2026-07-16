f129c1037662bf86bd67fb539fc4d950fe2b950b
smartcsie <smartcsie@gmail.com>
2026-07-16 10:55:31 +0800

    Add gemini comment

--- solution/Solution_1759.cpp
+++ solution/Solution_1759.cpp
@@ -17,13 +17,12 @@ class Solution {
 public:
     int countHomogenous(string s) {
         const int MOD = 1e9 + 7;
-        long ans = 0; 
-        long count = 1;
+        int ans = 0, count = 1;
         for (int i = 1; i <= s.size(); i++) {
             if (i < s.size() && s[i] == s[i - 1]) {
                 count++;
             } else {
-                ans = (ans + count * (count + 1) / 2) % MOD;
+                ans = (ans + (long long)count * (count + 1) / 2) % MOD;
                 count = 1;
             }
         }
