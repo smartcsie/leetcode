bd646a6676a060704fe9ad16fb136ddaadd98ad4
laurence <smartcsie@gmail.com>
2026-08-11 21:46:14 +0800

    Add solution for problem 0103

--- solution/Solution_0103.cpp
+++ solution/Solution_0103.cpp
@@ -14,22 +14,29 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        if (!root) return {};
-        vector<vector<int>> res;
-        queue<TreeNode*> q{{root}};
-        int cnt = 0;
-        while (!q.empty()) {
-            vector<int> oneLevel;
-            for (int i = q.size(); i > 0; --i) {
-                TreeNode *t = q.front(); q.pop();
-                oneLevel.push_back(t->val);
-                if (t->left) q.push(t->left);
-                if (t->right) q.push(t->right);
+        if(!root) return {};
+        deque<TreeNode*> q({root});
+        vector<vector<int>> ans;
+        while(!q.empty()) {
+            vector<int> level;
+            for(int i = q.size() - 1; i >= 0 ; i--) {
+                 TreeNode* node;
+                if(ans.size() % 2 == 0) {
+                    node = q.front();
+                    q.pop_front();
+                    level.push_back(node->val);
+                    if(node->left) q.push_back(node->left);
+                    if(node->right) q.push_back(node->right);
+                } else {
+                    node = q.back();
+                    q.pop_back();
+                    level.push_back(node->val);
+                    if(node->right) q.push_front(node->right);
+                    if(node->left) q.push_front(node->left);
+                }
             }
-            if (cnt % 2 == 1) reverse(oneLevel.begin(), oneLevel.end());
-            res.push_back(oneLevel);
-            ++cnt;
+            ans.push_back(level);
         }
-        return res;
+        return ans;
     }
 };
