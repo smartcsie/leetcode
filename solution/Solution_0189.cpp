00712f0c73179cb1020925670fd971ab61ef9982
laurence <smartcsie@gmail.com>
2026-09-02 22:19:28 +0800

    Add solution for problem 0189

--- solution/Solution_0189.cpp
+++ solution/Solution_0189.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        std::rotate(nums.begin(), nums.begin() + (nums.size() - k % nums.size()), nums.end());
+        k %= nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
