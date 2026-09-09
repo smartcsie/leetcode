c462d6d68337eefb812e468ff97f6103af21962f
laurence chang <laurence.chang@gallopwave.com>
2026-09-09 13:18:13 +0800

    Add solution for problem 0279

--- solution/Solution_0279.cpp
+++ solution/Solution_0279.cpp
@@ -15,7 +15,7 @@
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int> dp(n + 1, INT_MAX);
+        vector<int> dp(n + 1, n + 1);
         dp[0] = 0;
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j * j <= i; j++) {
