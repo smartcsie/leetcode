ddfaf3ec593fca9fd393a1f7d9cb41098db430aa
laurence <smartcsie@gmail.com>
2026-09-09 23:16:18 +0800

    Add solution for problem 0263

--- solution/Solution_0263.cpp
+++ solution/Solution_0263.cpp
@@ -17,12 +17,9 @@ class Solution {
 public:
     bool isUgly(int n) {
         if(n <= 0) return false;
-        for (int f : {2, 3, 5}) {
-            while (n % f == 0) {
-                n /= f;
-            }
+        for(int x : {2, 3, 5}) {
+            while(n % x == 0) n /= x;
         }
-
         return n == 1;
     }
 };
