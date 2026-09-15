0294cb4d7e637006a18febe9cead13e3ef5a3fa3
laurence chang <laurence.chang@gallopwave.com>
2026-09-15 12:55:01 +0800

    Add solution for problem 0628

--- solution/Solution_0628.cpp
+++ solution/Solution_0628.cpp
@@ -16,29 +16,26 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
-        int min1 = INT_MAX, min2 = INT_MAX ;
-        int n = nums.size();
-        for(const int& num : nums ) {
-            if(num > max1) {
-                max3 = max2;
-                max2 = max1;
-                max1 = num;
-            } else if(num > max2) {
-                max3 = max2;
-                max2 = num;
-            } else if(num > max3) {
-                max3 = num;
+        vector<int> mx(3, INT_MIN);
+        vector<int> mn(2, INT_MAX);
+        for(const int& num : nums) {
+            if(num > mx[0]) {
+                mx[2] = mx[1];
+                mx[1] = mx[0];
+                mx[0] = num;
+            } else if(num > mx[1]) {
+                mx[2] = mx[1];
+                mx[1] = num;
+            } else if(num > mx[2]) {
+                mx[2] = num;
             }
-
-            if(num < min1) { 
-                min2 = min1;
-                min1 = num;
-            } else if(num < min2) { 
-                min2 = num;
-            } 
+            if(num < mn[0]) {
+                mn[1] = mn[0];
+                mn[0] = num;
+            } else if(num < mn[1]) {
+                mn[1] = num;
+            }
         }
-       
-        return max(max1 * max2 * max3, max1 * min2 * min1);
+        return max(mx[0] * mx[1] * mx[2], mx[0] * mn[0] * mn[1]);
     }
 };
