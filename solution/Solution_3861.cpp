3f0f3f21049da013fcde8e705eadefc91abba997
laurence chang <laurence.chang@gallopwave.com>
2026-09-17 12:28:55 +0800

    Add solution for problem 3861

--- solution/Solution_3861.cpp
+++ solution/Solution_3861.cpp
@@ -13,19 +13,14 @@
 class Solution {
 public:
     int minimumIndex(std::vector<int>& capacity, int itemSize) {
-        int min_val = INT_MAX;
-        int min_index = -1;
-        
-        for (int i = 0; i < capacity.size(); i++) {
-            // 檢查是否足夠放入物品
-            if (capacity[i] >= itemSize) {
-                // 更新找到的最小值及其索引
-                if (capacity[i] < min_val) {
-                    min_val = capacity[i];
-                    min_index = i;
-                }
+        int ans = -1;
+        int mn = INT_MAX;
+        for(int i = 0; i < capacity.size(); i++) {
+            if(capacity[i] >= itemSize && capacity[i] < mn) {
+                mn = capacity[i];
+                ans = i;
             }
         }
-        return min_index;
+        return ans;
     }
 };
